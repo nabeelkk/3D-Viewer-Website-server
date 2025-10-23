fd518f1c35ac0f0fffe4744bb99b9342930257c7
nabeelkk <muhammednabeelkk@Muhammeds-MacBook-Air-2.local>
2025-10-23 10:20:13 +0530

    update cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || '*',
     credentials: true,
   })
 );
